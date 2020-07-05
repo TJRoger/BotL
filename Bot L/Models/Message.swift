171a88d052b44aa04652b857b1fcf51715f72df0
Roger Luo <tjrogertj@gmail.com>
2020-07-05 10:36:06 +0800

    add users and message model

---
+++ Bot L/Models/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Bot L
+//
+//  Created by Roger Luo on 2020/7/5.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import Foundation
---
+++ Bot L/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Bot L
+//
+//  Created by Roger Luo on 2020/7/5.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import Foundation
