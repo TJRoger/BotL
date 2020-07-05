d7c4c64b295eb3e5738a638d3899e4429ab0b4ed
Roger Luo <tjrogertj@gmail.com>
2020-07-05 10:05:57 +0800

    Initial Commit

---
+++ Bot LTests/Bot_LTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bot_LTests.swift
+//  Bot LTests
+//
+//  Created by Roger Luo on 2020/7/5.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import XCTest
+@testable import Bot_L
+
+class Bot_LTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
